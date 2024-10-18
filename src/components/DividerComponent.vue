6694d4e0d60b385ce7a8dc2dfb4cb388e090daba
Sandra Solberg <sandsol97@gmail.com>
2024-10-18 11:08:46 +0200

    Ini

---
+++ src/components/DividerComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <hr class="divider" />
+</template>
+
+<style scoped>
+.divider {
+  border: none;
+  border-top: 2px solid var(--color-heading); /* Adjust the color and width as needed */
+  margin: 0.5rem 0; /* Adjust the spacing above and below the divider */
+}
+</style>
---
+++ src/components/InfoBlock.vue
@@ -0,0 +1,26 @@
+<script setup>
+import DividerComponent from './DividerComponent.vue'
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div className="container">
+    <h2>{{ title }}</h2>
+    <DividerComponent />
+
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  text-align: center;
+}
+</style>
